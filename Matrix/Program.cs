31c3283764cbda5d914423f6250b8dd8f610a224
KristianBarzilov06 <kikogaming0@gmail.com>
2024-01-03 11:06:29 +0200

    Add project files.

---
+++ Matrix/Program.cs
@@ -0,0 +1,30 @@
+﻿using System.Security.Cryptography.X509Certificates;
+
+namespace Matrix
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Choose an operation:");
+            Console.WriteLine("1. Display random Matrix A and B, then Sum");
+            Console.WriteLine("2. OtherOperation");
+
+            string choice = Console.ReadLine();
+
+            SumMatrix sumMatrix = new SumMatrix();
+
+            switch (choice)
+            {
+                case "1":
+                    sumMatrix.Sum();
+                    break;
+                case "2":
+                    break;
+                default:
+                    Console.WriteLine("Invalid choice");
+                    break;
+            }
+        }
+    }
+}
---
+++ Matrix/SumMatrix.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matrix
+{
+    public class SumMatrix
+    {
+        double[,] A = { { 2, -1, 3 },
+                            { 4, 5, 6 },
+                            { 5, 6, 7 },
+        };
+        double[,] B = { { 4, -1, 2 },
+                            { 5, 6, 7 },
+                            { 6, 7, 8 },
+        };
+        public void DisplayMatrix(double[,] matrix)
+        {
+            int rows = matrix.GetLength(0);
+            int columns = matrix.GetLength(1);
+
+            for (int i = 0; i < rows; i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j < columns; j++)
+                {
+                    Console.Write(matrix[i, j]);
+                    if (j < columns - 1)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("}");
+                Console.WriteLine();
+            }
+            Console.WriteLine();
+        }
+
+        public void Sum()
+        {
+            Console.WriteLine("Matrix A:");
+            DisplayMatrix(A);
+            Console.WriteLine("Matrix B:");
+            DisplayMatrix(B);
+
+            Console.WriteLine("Press any key to continue...");
+            Console.ReadKey();
+
+            Console.WriteLine("Result:");
+            for (int i = 0; i < 3; i++)
+            {
+                Console.Write("{");
+                for (int j = 0; j < 3; j++)
+                {
+                    A[i, j] = A[i, j] + B[i, j];
+                    Console.Write(A[i, j]);
+                    if (j < 2)
+                    {
+                        Console.Write(" ");
+                    }
+                }
+                Console.Write("}");
+                Console.WriteLine();
+            }
+        }
+    }
+}
